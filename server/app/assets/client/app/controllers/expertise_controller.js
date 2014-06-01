20f1fdcee8c48b89156942000eb0e14116f004eb
Sasha <dimurchik@ukr.net>
2014-06-01 17:49:34 +0300

    add modal window for create new expertise

--- server/app/assets/client/app/controllers/expertise_controller.js
+++ server/app/assets/client/app/controllers/expertise_controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('expertise.controllers', []).
-	controller('expertiseController', function ($scope, $http, $cookieStore, $location, $cookies, $window) {
+	controller('expertiseController', function ($scope, $http, $cookieStore, $location, $cookies, $modal) {
 
 		$scope.expertise_list = [];
 		$scope.alnernative_list = [];
@@ -28,13 +28,6 @@ angular.module('expertise.controllers', []).
 			$scope.show_expertise = true;
 		}
 
-		$scope.save = function(name) {
-			$scope.expertise_list.push({ name: name });
-			$http.post("/expertizes.json", { name: name, method: "test" }).success(function(res){
-				console.log(res);
-			});
-		}
-
 		$scope.add_alternative = function() {
 			$scope.show_form_expertise = true;
 		}
@@ -47,10 +40,28 @@ angular.module('expertise.controllers', []).
 			$scope.expertiseName = item.name;
 		}
 
-		$scope.home = function () {
-			$location.path("/");
-			$window.location.reload();
-			window.location.reload();
-		}
+		$scope.openModalWindow = function () {
+			var modalInstance = $modal.open({
+				templateUrl: 'templates/add_expertise_modal.html',
+				controller: expertiseModalCtrl,
+				resolve: {
+					expertiseList: function () { return $scope.expertise_list; }
+				}
+			});
+		};
+
+		var expertiseModalCtrl = function ($scope, $modalInstance, expertiseList) {
+			$scope.save = function(name) {
+				expertiseList.push({ name: name });
+				$http.post("/expertizes.json", { name: name, method: "test" }).success(function(res){
+					$scope.cancel();
+				});
+			}
+
+			$scope.cancel = function () {
+				$modalInstance.dismiss('cancel');
+			};
+		};
+
 
 	});
